dfd7dbb20e11d992f8a57ad957ff8377d385dc37
vancece <vancece@outlook.com>
2020-12-16 12:01:10 +0800

    ^update^

--- homework/experiment_3/processData/processData.py
+++ homework/experiment_3/processData/processData.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     data_path = "../data/bank-additional-full.csv"
     data = pd.read_csv(data_path, sep=";")
     print(data.info())
+
+    for i in data.columns:
+        if type(data[i][0]) is str:
+            print("未知的列为：" + i + ": " + str(data[data[i] == "unknown"]["y"].count()))
